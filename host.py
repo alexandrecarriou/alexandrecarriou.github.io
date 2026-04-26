3aeeb648f120144719427a81b9d64d6bb86139f4
alexandrecarriou <carriou.e2501174@etud.univ-ubs.fr>
2026-04-26 19:49:48 +0200

    portfolio

---
+++ host.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class NoCacheHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
+server = HTTPServer(("0.0.0.0", 9000), NoCacheHandler)
+server.serve_forever()
